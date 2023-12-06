5781c1cf4840131eff2beedf38997c83073d0ec6
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-12-06 16:14:36 +0100

    add server

--- server/routes/users.js
+++ server/routes/users.js
@@ -41,16 +41,10 @@ users.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const users = await executeSQL(`SELECT * FROM users WHERE email='${email}';`);
   const data = users[0];
-<<<<<<< HEAD
-  if (!data) return console.error("User doesn't exist");
-  const token = jwt.sign({ data });
-  res.json(token);
-=======
   if (!data) return res.json({ error: "User doesn't exist" });
   if (data.password !== password) return res.json({ error: "Login failed" });
   const token = jwt.sign({ data });
   res.json({ token });
->>>>>>> 3d41c9ab08094cfd471b82f157b2bf986d09eb5f
 });
 
 module.exports = users;
