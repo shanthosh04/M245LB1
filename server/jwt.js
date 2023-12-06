3d41c9ab08094cfd471b82f157b2bf986d09eb5f
Keith50112e5 <keithyannick.hager01@gmail.com>
2023-12-06 15:42:38 +0100

    rename login to register added actual login

--- server/jwt.js
+++ server/jwt.js
@@ -6,18 +6,18 @@ const sign = (data) => jwt.sign(data, jwtSecret, { expiresIn: "1h" });
 const verifyMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) return console.error("No authorization header.");
+  if (!authorization) return res.json({ error: "No authorization header." });
 
   let user;
   try {
     user = jwt.verify(authorization, jwtSecret);
   } catch (err) {
-    return console.error(err.message);
+    return res.json({ error: err.message });
   }
 
   const { data } = user;
 
-  if (!data) return console.error("Invalid Token.");
+  if (!data) return res.json({ error: "Invalid Token." });
 
   res.data = data;
 
