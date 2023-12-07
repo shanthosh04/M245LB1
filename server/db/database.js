e65febc9edca58af5377a4d90d3e785b18476940
Keith50112e5 <keithyannick.hager01@gmail.com>
2023-12-07 14:05:55 +0100

    changed database packages again to knex

--- server/db/database.js
+++ server/db/database.js
@@ -3,28 +3,33 @@ const fs = require("fs");
 let conn = null;
 
 const dbInit = async () => {
-  conn = require("mysql").createConnection({
-    database: process.env.DB_NAME || "mydb",
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "user",
-    password: process.env.DB_PW || "123456",
+  conn = require("knex")({
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST || "localhost",
+      port: process.env.DB_PORT || 3306,
+      user: process.env.DB_USER || "user",
+      password: process.env.DB_PW || "123456",
+      database: process.env.DB_NAME || "mydb",
+    },
   });
-  conn.connect();
 
-  fs.readFile("./server/db/init.sql", "utf-8", async (err, data) => {
+  fs.readFile("./server/db/init.sql", "utf-8", (err, data) => {
     if (err) return console.error(err);
     const queries = data.split(";");
     queries.pop();
-    queries.forEach(async (query) => await executeSQL(query));
+    queries.forEach((query) => executeSQL(query));
   });
 };
 
-const executeSQL = async (query) => {
+const executeSQL = (query) => {
   let output = "";
-  conn.query(query, (err, results) => {
-    if (err) return console.error(err);
-    output = results;
-  });
+  conn
+    .raw(query)
+    .then((response) => {
+      output = response;
+    })
+    .catch((err) => console.error(err));
   return output;
 };
 
