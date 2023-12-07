54dd672559837e2c1cf8b9153a82cc60cae9ee93
Keith50112e5 <keithyannick.hager01@gmail.com>
2023-12-07 11:59:29 +0100

    changed db schema structure

--- server/db/init.sql
+++ server/db/init.sql
@@ -10,7 +10,23 @@ CREATE TABLE IF NOT EXISTS users (
     phone VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
-  );
+);
+
+CREATE TABLE IF NOT EXISTS rooms (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    img BLOB,
+    desk VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS parkings (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    img BLOB,
+    desk VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS roles (
     id INT NOT NULL AUTO_INCREMENT,
@@ -18,15 +34,28 @@ CREATE TABLE IF NOT EXISTS roles (
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user) REFERENCES users(id)
-  );
+);
 
-CREATE TABLE IF NOT EXISTS reservations (
+CREATE TABLE IF NOT EXISTS room_reservations (
     id INT NOT NULL AUTO_INCREMENT,
-    user int,
-    property VARCHAR(255) NOT NULL,
+    user INT,
+    room INT,
     status VARCHAR(255) NOT NULL,
-    reserved_from VARCHAR(255) NOT NULL,
-    reserved_to VARCHAR(255) NOT NULL,
+    reserved_from INT NOT NULL,
+    reserved_to INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user) REFERENCES users(id)
-  );
+    FOREIGN KEY (user) REFERENCES users(id),
+    FOREIGN KEY (room) REFERENCES rooms(id)
+);
+
+CREATE TABLE IF NOT EXISTS parking_reservations (
+    id INT NOT NULL AUTO_INCREMENT,
+    user INT,
+    parking INT,
+    status VARCHAR(255) NOT NULL,
+    reserved_from INT NOT NULL,
+    reserved_to INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user) REFERENCES users(id),
+    FOREIGN KEY (parking) REFERENCES parkings(id)
+);
