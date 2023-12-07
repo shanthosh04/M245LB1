5caf44b023c269165463e88f72ecdeeb2d5a7bb6
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-12-07 13:18:51 +0100

    change

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 const ip = process.env.IP || "localhost";
 const url = `http://${ip}:${port}/`;
 
